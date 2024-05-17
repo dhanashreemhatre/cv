8ed8c8a21b347ace7ac882de71891adcc96ca7b1
dhanashree-webdev <aetos.collection@gmail.com>
2024-05-17 17:33:44 +0530

    loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+"use client"
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import MainContent from './Components/MainContent';
 import SideBar from './Components/SideBar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes component
@@ -6,8 +8,33 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Im
 import ProjectDetail from './Components/sections/ProjectDetail';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading time with setTimeout
+    const timer = setTimeout(() => setLoading(false), 3000);
+
+    // Clear the timer on component unmount to prevent memory leaks
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <Router>
+    {loading ? ( // Render the loader if loading state is true
+      <div id="loader" className='h-screen flex justify-center items-center bg-black'>
+        <div className="waves">
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        </div>
+      </div>
+    ) : ( // Render the main content once loading is complete
       <div className='dark:bg-black'>
         <div className='relative min-h-screen'>
           <div className='grid grid-cols-1 sm:grid-cols-2'>
@@ -15,15 +42,16 @@ function App() {
               <SideBar />
             </div>
             <div>
-              <Routes> {/* Define all your routes inside a single Routes component */}
-                <Route path="/projects/:projectId" element={<ProjectDetail />} /> {/* Specify the component to render using the element prop */}
-                <Route path="/" element={<MainContent />} /> {/* Set up a Route for the root path */}
+              <Routes>
+                <Route path="/projects/:projectId" element={<ProjectDetail />} />
+                <Route path="/" element={<MainContent />} />
               </Routes>
             </div>
           </div>
         </div>
       </div>
-    </Router>
+    )}
+  </Router>
   );
 }
 
